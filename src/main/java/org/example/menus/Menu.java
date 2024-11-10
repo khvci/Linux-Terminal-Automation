0065d53f84b3cef4d754c58cd5f4b825a6852af1
khvci <88493840+khvci@users.noreply.github.com>
2024-11-10 18:03:42 +0300

    add abstract func show()

--- src/main/java/org/example/menus/Menu.java
+++ src/main/java/org/example/menus/Menu.java
@@ -2,6 +2,7 @@ package org.example.menus;
 
 import java.util.Scanner;
 
-public class Menu {
+public abstract class Menu {
     static Scanner scanner = new Scanner(System.in);
+    public abstract void show();
 }
